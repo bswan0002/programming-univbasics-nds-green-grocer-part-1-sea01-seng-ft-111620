de9126f76ef0b413c8d94977e5cef736ee4588af
Ben Swanson <bswan0002@gmail.com>
2020-11-14 01:15:41 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,20 +1,17 @@
 def find_item_by_name_in_collection(name, collection)
-  collection.select { |itemHash| return itemHash if itemHash[:item] == name }
-  return nil
+  collection.find { |itemHash| itemHash[:item] == name }
 end
 
 def consolidate_cart(cart)
 
   newCart = []
   cart.each do |itemHash|
-    if find_item_by_name_in_collection(itemHash[:item], newCart)
-      find_item_by_name_in_collection(itemHash[:item], newCart)[:count] += 1
+    currentItem = find_item_by_name_in_collection(itemHash[:item], newCart)
+    if currentItem
+      currentItem[:count] += 1
     else
       newCart << itemHash
       newCart.last[:count] = 1
     end
   end
 end
-
-
-  
